c38af5d770c3acee96d873fdf06ac19ef642b13a
janrasel2121 <158441689+janrasel2121@users.noreply.github.com>
2024-11-20 10:41:48 +0800

    Add files via upload

---
+++ CPESCDO/script.js
@@ -0,0 +1,4 @@
+document.querySelector(".menu").addEventListener('click', function(){
+    document.querySelector("nav").classList.toggle("active");
+    document.querySelector(".links").classList.toggle("active");
+})
