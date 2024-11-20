a68a177e72e550d4d5f59064e37829e33e09299c
janrasel2121 <158441689+janrasel2121@users.noreply.github.com>
2024-11-20 10:43:29 +0800

    Delete CPESCDO directory

--- CPESCDO/script.js
+++
@@ -1,4 +0,0 @@
-document.querySelector(".menu").addEventListener('click', function(){
-    document.querySelector("nav").classList.toggle("active");
-    document.querySelector(".links").classList.toggle("active");
-})
